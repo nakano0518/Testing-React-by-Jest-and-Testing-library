6a9d8a8275899c5c87d7322d930d4d55779bea71
nakano0518 <m15sc018.prog@gmail.com>
2020-10-05 05:07:32 +0900

    Integration Testing component to Redux AsyncThunk API

--- src/ReduxAsync.js
+++ src/ReduxAsync.js
@@ -2,16 +2,21 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   selectCount,
+  selectUsername,
   fetchDummy,
+  fetchJSON,
 } from "./features/customCounter/customCounterSlice";
 
 const ReduxAsync = () => {
   const count = useSelector(selectCount);
+  const username = useSelector(selectUsername);
   const dispatch = useDispatch();
   return (
     <div>
       <span data-testid="count-value">{count}</span>
       <button onClick={() => dispatch(fetchDummy(5))}>fetchDummy</button>
+      {username && <h1>{username}</h1>}
+      <button onClick={() => dispatch(fetchJSON())}>fetchJSON</button>
     </div>
   );
 };
